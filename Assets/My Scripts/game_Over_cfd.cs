205a1d04f4ab2354fd154879f0a2ed4d5cd43254
sachinjain3677 <sachinjain3677@gmail.com>
2018-02-25 16:06:26 +0530

    merged both the scenes in one single project

---
+++ Assets/My Scripts/game_Over_cfd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_Over_cfd : MonoBehaviour {
+
+	BombSpawnAndExplode_cfd bsae;
+
+	public GameObject game_over_menu;
+	// Use this for initialization
+	void Start () {
+		bsae = GameObject.Find ("GameController").GetComponent<BombSpawnAndExplode_cfd> ();
+		game_over_menu.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bsae.isDead) {
+			game_over_menu.SetActive (true);
+		}
+	}
+}
