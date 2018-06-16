e4350ed9ae3ee86356f5e980db4fe87e5fcda0e0
Chaira19 <chirayuasati19@gmail.com>
2018-06-16 12:32:30 +0530

    NonARMode

--- Assets/My Scripts/canvascontroller.cs
+++ Assets/My Scripts/canvascontroller.cs
@@ -6,9 +6,19 @@ public class canvascontroller : MonoBehaviour {
 
 	public GameObject readyCanvas;
 	public GameObject startCanvas;
+    public GameObject panel;
+    public GameObject username;
+    public GameObject submit;
 
 	public void switchCanvas () {
 		startCanvas.SetActive(true);
 		readyCanvas.SetActive(false);
 	}
+
+    public void switchwithin()
+    {
+        panel.SetActive(true);
+        username.SetActive(false);
+        submit.SetActive(false);
+    }
 }
